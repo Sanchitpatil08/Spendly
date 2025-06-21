7327f64bf5e0d2748a6a036a00f720265d777cca
Sanchit <sanchitpatil81004@gmail.com>
2025-06-21 17:10:58 +0530

    Updated the necessary changed
    Updated the few Ui changes

--- components/ui/resizable-navbar.jsx
+++ components/ui/resizable-navbar.jsx
@@ -67,7 +67,7 @@ export const NavBody = ({
         minWidth: "800px",
       }}
       className={cn(
-        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-3 lg:flex dark:bg-transparent",
+        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-3 py-2 lg:flex dark:bg-transparent",
         visible && "bg-blue dark:bg-neutral-950/80",
         className
       )}>
@@ -195,7 +195,7 @@ export const NavbarLogo = () => {
       href=""
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black">
       <img
-        src="logonew.png"
+        src="bg-removed-logo.png"
         alt="logo"
         width={40}
         height={0} />
